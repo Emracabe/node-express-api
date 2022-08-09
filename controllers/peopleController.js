7d76e0265a5f80acafc6185bb4ca97fcbf3c30cd
iMAC 16 MAC Lab <imac16maclab@iMACs-iMac-10.local>
2022-08-09 15:37:34 +0800

    Created getPerson() and createPerson() in the peopleControllers

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -1,8 +1,18 @@
 let { people } = require("../data");
 
 const getPeople = (req, res) => {
-  res.status(200).send({ sucess: true, data: people });
+  res.status(200).json({ sucess: true, data: people });
 };
 
+const createPerson = (req, res) => {
+    const {name} = req.body;
+
+    if (!name) {
+        return res.status(404).json({success: false, msg: 'please provide some credentials'})
+    }
+
+    people.push({id: people.length + 1, name: name});
+    res.status(201).json({success: true, data: people});
+}
 
 module.exports = {getPeople}
